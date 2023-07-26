099943ca54945265e69eaa8aa118883abe0d540b
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2023-07-26 19:33:14 +0700

    Add files via upload

---
+++ build.py
@@ -0,0 +1,33 @@
+import os,sys,shutil
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+class Main:
+	def __init__(self):
+		self.file = "zmbf.cpp"
+		
+	def build(self):
+		try:
+			nama = self.file.split(".")[0]
+			setup(
+				name = self.file,
+				ext_modules = cythonize(Extension(name=nama, sources=[self.file])),
+				script_args = ["build_ext", "--inplace", "--force", "-j 5"]
+			)
+			self.clear()
+		except:
+			exit(" Terjadi Kesalahan, Silahkan Install Ulang Script Sesuai Petunjuk")
+		
+	def clear(self):
+		try: shutil.rmtree("build/")
+		except:pass
+		try: os.remove(self.file)
+		except:pass
+		self.create_run()
+			
+	def create_run(self):
+		with open("run.py","w") as run:
+			run.write("from zmbf import Menu\nMenu().menu()")
+		exit(" Selesai Menginstall, Silahkan Ketik : python run.py")
+	
+Main().build()
